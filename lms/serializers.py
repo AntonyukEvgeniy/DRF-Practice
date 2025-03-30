b063e0e66009cf46fab845f354dc38e713b684dc
Антонюк Евгений <38914583+AntonyukEvgeniy@users.noreply.github.com>
2025-03-30 23:44:09 +0300

    Вложенный сериализатор

--- lms/serializers.py
+++ lms/serializers.py
@@ -3,17 +3,20 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = ["id", "course", "title", "description", "preview", "video_url"]
+
 class CourseSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
     class Meta:
         model = Course
-        fields = ["id", "title", "preview", "description", "lessons_count"]
+        fields = ["id", "title", "preview", "description", "lessons_count","lessons"]
 
     def get_lessons_count(self, obj):
         return obj.lessons.count()
 
 
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
-        fields = ["id", "course", "title", "description", "preview", "video_url"]
